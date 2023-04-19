a0d0c58e6a868189c7fd857a6d92a1e37f921be4
Mikhail Katsuba <74879271+katsuba-mikhail@users.noreply.github.com>
2023-04-19 17:53:57 +0300

    add parameters

--- dags/dag_python_operator.py
+++ dags/dag_python_operator.py
@@ -8,18 +8,24 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-def hello():
-    print('hello world!')
+def hello(name, age):
+    print(f'Hello world!'
+          f'My name is {name}'
+          f'I am {age} years old.')
 
 with DAG(
     default_args=default_args,
-    dag_id='dag_python_operator_v1',
+    dag_id='dag_python_operator_v2',
     start_date=datetime(2023, 4, 12),
     schedule_interval='@daily',
+    catchup=False,
 
 ) as dag:
 
     task1 = PythonOperator(
         task_id='hello',
         python_callable=hello,
+        op_kwargs={'name': 'Hihi', 'age': '22'}
     )
+
+    task1
